94aacfda3301be130c78a154c63f7a53e498b76f
Damian Epsztein <damiepsz3@gmail.com>
2016-06-20 10:48:14 -0300

    primer commit con el tp hasta el lab2

---
+++ Entidades/Entidades.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Entidades
+    {
+        public Entidades(){
+        this.state = new States();
+    }
+        public int id { get; set; }
+        public States state { get; set; }
+
+        public enum States { 
+        Deleted,
+        New,
+        Modified,
+        Unmodified,
+        }
+    }
+}
