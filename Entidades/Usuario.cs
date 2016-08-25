11491b059420a69492a1590e553c1700b08b10d6
Damian Epsztein <damiepsz3@gmail.com>
2016-08-25 09:10:27 -0300

    avance lab03

--- Entidades/Usuario.cs
+++ Entidades/Usuario.cs
@@ -9,6 +9,8 @@ namespace Entidades
     public class Usuario : Entidades
     {
         public string NombreUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
         public string Clave { get; set; }
         public string Email { get; set; }
         public bool Habilitado { get; set; }
